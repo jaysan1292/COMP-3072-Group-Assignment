5458f41cfb974d69f9993997233ea4b0fa4ea065
Jason Recillo <jaysan1292@gmail.com>
2013-03-08 07:56:54 -0500

    Added redirect function

--- authenticate.php
+++ authenticate.php
@@ -10,12 +10,14 @@ $success = $auth->authenticateUser($user, $pass, $login);
 if($success) {
     session_start();
     $_SESSION['current_user'] = $login;
-    // Redirect to home page
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . '/test.php');
+    $_SESSION['logged_in'] = TRUE;
+
+    redirect_to_page(HOME_PAGE);
+    // header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . '/test.php');
 } else {
     // TODO: I really don't like the fact that the message is sent in the query string... it's a little thing, but it's too easy for the user to modify :p
 
-    // Redirect to login screen, with an error message
     $errmsg = urlencode('Incorrect username or password.');
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . "/index.php?msg=$errmsg");
+    redirect_to_page(LOGIN_PAGE . "?msg=$errmsg");
+    // header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . "/index.php?msg=$errmsg");
 }
--- php/functions.php
+++ php/functions.php
@@ -22,6 +22,27 @@ function to_rectangle($bbox) {
     return new Rectangle($x, $y, $width, $height);
 }
 
+/*
+ * Redirects to the specified page. Please note, however,
+ * that it will only redirect to pages within the application
+ * (i.e., anything under ROOT_DIR). To redirect elsewhere,
+ * just set the location header yourself.
+ */
+function redirect_to_page($page) {
+    $vars = get_defined_vars();
+    if(!array_key_exists('page', $vars) ||
+       is_null($page) ||
+       empty($page)) {
+        throw new Exception('No page specified', 1);
+    }
+    // if passed in string does not contain ROOT_DIR
+    if(strpos($page, ROOT_DIR) !== FALSE) {
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . $page);
+    } else {
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . $page);
+    }
+}
+
 function time24_to_string($time) {
     $time = str_replace(':', '', $time);
     $min = substr($time, -2);

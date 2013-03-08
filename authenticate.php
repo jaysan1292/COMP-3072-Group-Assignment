7929377cf5c406a452b81cd38f4bd6b9af77b4e7
Jason Recillo <jaysan1292@gmail.com>
2013-03-08 01:22:05 -0500

    index.php is the "login page", so redirect there on failure

--- authenticate.php
+++ authenticate.php
@@ -11,11 +11,11 @@ if($success) {
     session_start();
     $_SESSION['current_user'] = $login;
     // Redirect to home page
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . '/index.php');
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . '/test.php');
 } else {
     // TODO: I really don't like the fact that the message is sent in the query string... it's a little thing, but it's too easy for the user to modify :p
 
     // Redirect to login screen, with an error message
     $errmsg = urlencode('Incorrect username or password.');
-    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . "/test.php?msg=$errmsg");
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . ROOT_DIR . "/index.php?msg=$errmsg");
 }

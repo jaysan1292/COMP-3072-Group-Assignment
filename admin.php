16acac1fed0f0cc0bd55bfd45845074400ec3674
Jason Recillo <jaysan1292@gmail.com>
2013-03-20 10:26:45 -0400

    Replaced backslashes with forward slashes

--- admin.php
+++ admin.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'php\global.php';
+require_once 'php/global.php';
 ?>
 <!doctype html>
 <html>
--- professor(old).php
+++ professor(old).php
@@ -1,5 +1,5 @@
 <?php
-require_once 'php\global.php';
+require_once 'php/global.php';
 session_start();
 ?>
 <!doctype html>

8f4edf0349b64143f9db6aa5c0d886e721df69cc
Jason Recillo <jaysan1292@gmail.com>
2013-04-12 11:25:02 -0400

    Time-off requests are now pulled from the database

--- aRequest.php
+++ aRequest.php
@@ -17,28 +17,9 @@
 				<td><?=$request['Professor']?></td>
 				<td><?=$request['Date']?></td>
 				<td><?=$request['Reason']?></td>
-				<td><a href="#" class="decision" data-type="select" data-pk="1" data-url="" data-original-title="Make a decision"></a></td>
+				<td><a href="#" class="decision" data-type="select" data-pk="<?=$request['StatusId']?>" data-url="" data-original-title="Make a decision"></a></td>
 			</tr>
 			<?php endforeach; ?>
-			<!-- <tr class="warning">
-				<td>Professor 2</td>
-				<td>getDates()</td>
-				<td>getReason()</td>
-				<td><a href="#" class="decision" data-type="select" data-pk="1" data-url="" data-original-title="Make a decision"></a></td>
-			</tr>
-			<tr class="error">
-				<td>Professor 3</td>
-				<td>getDates()</td>
-				<td>getReason()</td>
-				<td><a href="#" class="decision" data-type="select" data-pk="1" data-url="" data-original-title="Make a decision"></a></td>
-			</tr>
-			<tr class="info">
-				<td>Professor 4</td>
-				<td>getDates()</td>
-				<td>getReason()</td>
-				<td ><a href="#" class="decision" data-type="select" data-pk="1" data-url="" data-original-title="Make a decision"></a></td>
-			</tr> -->
-
 		</tbody>
 	</table>
 

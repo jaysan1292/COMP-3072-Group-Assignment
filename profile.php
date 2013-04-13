56e3e12b460d05fb76847acd0df29409cc9560a5
Jason Recillo <jaysan1292@gmail.com>
2013-04-12 21:33:37 -0400

    Moved the contents of editrecord.js into profile.php, since I need to use PHP to manipulate the javascript on the server side.

--- profile.php
+++ profile.php
@@ -14,7 +14,6 @@
 								</div>
 							</div>
 						</div>
-						<script src="editecord.js"></script>
 						<div class="span9">
 							<form class="form-inline">
 								<table id="user" class="table table-bordered table-striped">
@@ -61,4 +60,53 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</div>	</div>
+</div>
+<script src="editrecord.js">
+$(function(){
+	$.fn.editable.defaults.mode = 'inline';
+	$('#contact_number').editable({
+		url: '',
+		title: 'Contact Info:'
+	});
+	$('#email').editable({
+		url: '',
+		title: 'Email Address:'
+	});
+	// $('#department').editable({
+	// 	url: '',
+	// 	title: 'Department:'
+	// });
+	$('#department').editable({
+		value: 4,
+		source: [
+		{value: 1, text: 'T127'},
+		{value: 2, text: 'T141'},
+		{value: 3, text: 'Free-Agent'},
+		{value: 4, text: 'Rockstar'}
+		]
+	});
+	$('#courses').editable({
+        value: [1,2,3,4],
+        source: [
+              {value: 1, text: 'option1'},
+              {value: 2, text: 'option2'},
+              {value: 3, text: 'option3'},
+              {value: 4, text: 'option4'}
+           ]
+    });
+	$('#book_off_reason').editable({
+        url: 'post.php',
+        title: 'Enter comments',
+        rows: 5
+    });
+    $('#date1').combodate({
+    minYear: 2013,
+    maxYear: 2050,
+    });
+	$('#date2').combodate({
+    minYear: 2013,
+    maxYear: 2050,
+    });
+});
+</script>
